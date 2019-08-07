2a75f9f7e35baea0892d975d24f926715d859bc9
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-08-07 13:29:27 -0500

    function exercises

--- js/bonus_functions.js
+++ js/bonus_functions.js
@@ -1,38 +1,126 @@
-"use strict";
+(function () {
+
+    "use strict";
 
 //
 // Function Drills
 // Functions using conditionals but not loops or arrays:
 //
 //     Make a function named isOdd(number)
-
-function isOdd(num){
-    return num%2 === 1;
-}
-
-console.log(isOdd(9), 9, 'odd');
-console.log(isOdd(8), 8, 'odd');
+//
+// function isOdd(num){
+//     return num%2 === 1;
+// }
+//
+// console.log(isOdd(9), 9, 'odd');
+// console.log(isOdd(8), 8, 'odd');
 
 // Make a function named isEven(number)
-function isEven(num){
-    return num%2 === 0;
-}
-
-console.log(isEven(9), 9, 'even');
-console.log(isEven(8), 8, 'even');
+// function isEven(num){
+//     return num%2 === 0;
+// }
+//
+// console.log(isEven(9), 9, 'even');
+// console.log(isEven(8), 8, 'even');
 
 // Make a function named identity(input) that returns the input exactly as provided.
-//     Make a function named isFive(input)
+
+// function identity (input){
+//     return input;
+// }
+//
+// console.log(identity("Hello"), "Hello");
+// console.log(identity(5), 5);
+
+// Make a function named isFive(input)
+
+// Don't modify the following line, it generates a random number between 1 and 3
+// and stores it in a variable named random
+// var random = Math.floor((Math.random() * 3) + 1);
+//
+// function isFive(input){
+//     return input === 5;
+//
+// }
+//
+// console.log(random,'equal to five', isFive(random));
+// console.log(45,'equal to five', isFive(45));
+// console.log(5,'equal to five', isFive(5));
+
+
 // Make a function named addFive(input) that adds five to some input.
-//     Make a function named isMultipleOfFive(input)
+
+// Make a function named isMultipleOfFive(input)
+// var random = Math.floor((Math.random() * 3) + 1);
+//
+    function isMultipleOfFive(input) {
+        return input % 5 === 0;
+
+    }
+
+//
+// console.log(random,'multiple of five', isMultipleOfFive(random));
+// console.log(45,'multiple of five', isMultipleOfFive(45));
+
+
 // Make a function named isThree(input)
+// function isThree(num){
+//     return num ===3;
+// }
+//
+// console.log(5, 'is equal to 3' ,isThree(5));
+// console.log(3, 'is equal to 3' ,isThree(3));
+
 // Make a function named isMultipleOfThree(input)
+
+    function isMultipleOfThree(input) {
+        return input % 3 === 0;
+
+    }
+
+    console.log(9, 'is multiple of three', isMultipleOfThree(9));
+    console.log(5, 'is multiple of three', isMultipleOfThree(5));
+
 // Make a function named isMultipleOfThreeAndFive(input)
+
+    function isMultipleOfThreeAndFive(input) {
+        return isMultipleOfFive(input) && isMultipleOfThree(input);
+    }
+
+    console.log(15, ' is multiple of 3 and 5 ', isMultipleOfThreeAndFive(15));
+    console.log(20, ' is multiple of 3 and 5 ', isMultipleOfThreeAndFive(20));
+
 // Make a function named isMultipleOf(target, n) which checks if target is evenly divisible by n
+    function isMultipleOf(target, n) {
+        return target % n === 0;
+    }
+
+    console.log(10, 'is multiple of', 3, '?', isMultipleOf(10, 3));
+    console.log(15, 'is multiple of', 3, '?', isMultipleOf(15, 3));
+
 // Make a function named isTrue(boolean)
+
+    function isTrue(boolean) {
+        return boolean == true;
+    }
+
+    console.log(true, 'is true ', isTrue(true));
+    console.log(false, 'is true ', isTrue(false));
+
 // Make a function named isFalse(boolean)
+
+    function isFalse(boolean) {
+        return boolean == false;
+    }
+
+    console.log(true, 'is false ', isFalse(true));
+    console.log(false, 'is false ', isFalse(false));
+
 // Make a function named isTruthy(input), remember that values other than true will behave like true
+
+
 // Make a function named isFalsy(input), remember that values other than false behave like false
+
 // Make a function named isVowel(letter)
 // Make a function named isConsonant(letter)
 // Make a function named isCapital(letter)
@@ -68,6 +156,8 @@ console.log(isEven(8), 8, 'even');
 // Write a function called reverseString(string) that reverses a string
 // Make a function named absoluteValue(number) that returns the absolute value of a number.
 //     Make a function named rollDice(sides) that takes in an argument containing the number of sides the die should have. Generate a random number between 1 up to and including the number of sides.
+
+
 //     Simple Function Drills
 // Make a function called returnTwo() that returns the number 2 when called
 // Test this function with console.log(returnTwo())
@@ -88,6 +178,7 @@ console.log(isEven(8), 8, 'even');
 //     Make a function called sayString() which returns the string input passed in.
 // Test this function with console.log(sayString('codeup'))
 //
+
 //     Challenge Function Drills
 // Write a function called identity(input) that takes in an argument called input and returns that input.
 //
@@ -121,6 +212,8 @@ console.log(isEven(8), 8, 'even');
 //
 // Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter is the name of the math function you want to apply. a and b are the two numbers to run that function on.
 //
+
+
 // Even More Function Bonuses
 // Create a function that will return how many whitespace characters are at the beginning and end of a string.
 //
@@ -131,13 +224,18 @@ console.log(isEven(8), 8, 'even');
 //     If the second string input is not present in the first, return the first string as entered in the function.
 // Create a function that takes in a string and returns true if the last letter is an "a" (otherwise, return false).
 //
+
+
 // EXTRA CHALLENGE: create a function that will return how many whitespace characters are at the beginning of a string (hint: use regex).
 //
 // Create a function returnTrueMessage() that returns the string "Hey, it's true!"
 //
 // Create a function returnFalseMessage() that returns the string "Hey, it's false!"
 // Create a function returnMessage() that takes in a function and returns the call to the function
-// Experiement passing in different functions.
+// Experiment passing in different functions.
 //     Create a function, willLoginUser() that takes in a username string, password string, user age, a boolean indicating if they are an admin.
 //
 //     The function will return true if the username is not the same as the password and the user is at least 18 years old. If the user is an admin, they do not have to be a certain age but the password must still not match the username.
+
+
+})();
