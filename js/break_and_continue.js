18e4fabc53baebaf22d4f4088f3b1fd7cbbf5aab
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-08-12 15:13:23 -0500

    for loop

---
+++ js/break_and_continue.js
@@ -0,0 +1,16 @@
+
+
+function isOdd(num){
+    return num%2 === 1;
+}
+
+var userInput = prompt("Please enter an odd number between 1 and 50");
+
+console.log('Number to skip is: ' + userInput);
+for(var i = 1; i <= 50; i+=2){
+    if (i === parseFloat(userInput)) {
+        console.log('Yikes! Skipping number: '+ userInput);
+        continue;
+    }
+    console.log('Here is an odd number: ' + i);
+}
---
+++ js/for_loops.js
@@ -0,0 +1,40 @@
+function showMultiplicationTable(input) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(input + ' x ' + i + ' = ' + parseFloat(input) * i);
+    }
+}
+
+showMultiplicationTable(5)
+showMultiplicationTable(7);
+
+function isOdd(num) {
+    return num % 2 === 1;
+}
+
+
+var randomNumber; // = Math.floor(Math.random() * 200) + 20;
+
+for (var i = 0; i < 10; i++) {
+    randomNumber = Math.floor(Math.random() * 200) + 20;
+    if (isOdd(randomNumber)) {
+        console.log(randomNumber + " is odd");
+    } else {
+        console.log(randomNumber + " is even");
+    }
+}
+
+var str = "";
+
+for (var i = 1; i < 10; i++) {
+    for (var j = 0; j < i; j++) {
+        str += i;
+        //console.log(i);
+
+    }
+    console.log(str);
+    str = "";
+}
+
+for (var k = 100; k>0; k-=5){
+    console.log(k);
+}
