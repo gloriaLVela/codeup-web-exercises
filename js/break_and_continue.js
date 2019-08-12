d085965714f4716c79b3948a03d45999ef3a6b78
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-08-12 15:39:33 -0500

    break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,7 +4,7 @@ function isOdd(num){
     return num%2 === 1;
 }
 
-var userInput = prompt("Please enter an odd number between 1 and 50");
+var userInput = prompt("Please enter an odd number between 1 and 50 ");
 
 console.log('Number to skip is: ' + userInput);
 for(var i = 1; i <= 50; i+=2){
