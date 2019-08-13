cb402f4ab43ddc906a26e0d47439124f8529e2e7
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-08-13 09:00:12 -0500

    fix break

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,19 @@
-
-
-function isOdd(num){
-    return num%2 === 1;
+function isOdd(num) {
+    return num % 2 === 1;
 }
 
-var userInput = prompt("Please enter an odd number between 1 and 50 ");
+var userInput = 0;
+
+
+while (!isOdd(userInput)) {
+    userInput = +prompt("Please enter an odd number between 1 and 50 ");
+}
+
 
 console.log('Number to skip is: ' + userInput);
-for(var i = 1; i <= 50; i+=2){
+for (var i = 1; i <= 50; i += 2) {
     if (i === parseFloat(userInput)) {
-        console.log('Yikes! Skipping number: '+ userInput);
+        console.log('Yikes! Skipping number: ' + userInput);
         continue;
     }
     console.log('Here is an odd number: ' + i);
