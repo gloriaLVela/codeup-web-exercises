0757a066c1f1783237d881b40396c1172271274a
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-08-14 10:50:45 -0500

    fix

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,47 +2,37 @@
     "use strict";
 
 
-function isOdd(num) {
-    return num % 2 === 1;
-}
-<<<<<<< HEAD
-
-var userInput = 0;
-=======
->>>>>>> loops-exercise
-
-var userInput;
-
-<<<<<<< HEAD
-while (!isOdd(userInput)) {
-    userInput = +prompt("Please enter an odd number between 1 and 50 ");
-=======
-while (true) {
-    userInput = +prompt("Please enter an odd number between 1 and 50 ");
-    if (isOdd(userInput) && (+userInput > 1 && +userInput < 50)) break;
->>>>>>> loops-exercise
-}
-
-
-console.log('Number to skip is: ' + userInput);
-for (var i = 1; i <= 50; i += 2) {
-    if (i === parseFloat(userInput)) {
-        console.log('Yikes! Skipping number: ' + userInput);
-        continue;
+    function isOdd(num) {
+        return num % 2 === 1;
     }
-    console.log('Here is an odd number: ' + i);
-}
+
+    var userInput;
+
+    while (true) {
+        userInput = +prompt("Please enter an odd number between 1 and 50");
+        if (isOdd(userInput) && (+userInput > 1 && +userInput < 50)) break;
+    }
+
+
+    console.log('Number to skip is: ' + userInput);
+    for (var i = 1; i <= 50; i += 2) {
+        if (i === parseFloat(userInput)) {
+            console.log('Yikes! Skipping number: ' + userInput);
+            continue;
+        }
+        console.log('Here is an odd number: ' + i);
+    }
 
 // For each example
 
-var shapes = ['square', 'rectangle', 'circle', 'triangle'];
-var lastOne = 'last';
+    var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+    var lastOne = 'last';
 
 // loop through the array and log the values
-shapes.forEach(function(shape) {
-    console.log('Here is a lovely shape: ' + shape + '.');
-    lastOne = shape;
-});
+    shapes.forEach(function(shape) {
+        console.log('Here is a lovely shape: ' + shape + '.');
+        lastOne = shape;
+    });
 
-console.log('The last shape is ' + lastOne);
+    console.log('The last shape is ' + lastOne);
 })();
