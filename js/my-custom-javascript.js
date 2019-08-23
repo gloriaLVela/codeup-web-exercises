7d43b7dd7858164b99943ccd6a16d57ede1a9cc6
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-08-23 12:45:01 -0500

    Class Selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,17 +2,19 @@
 
 $(document).ready(function () {
 
-    // INSERT JAVASCRIPT CODE SAMPLE HERE
-    //alert('Ready');
-    $('h1').css('font-size', '40px');
-    $('.ul-class').css('color', 'green');
-    // var value = $('#tittleNew').val();
-    // alert(value);
-    var multipleValues = $( "#multiple1" ).val() || [];
-    alert(multipleValues);
-    // When using jQuery 3:
-    // var multipleValues = $( "#multiple" ).val();
-    $( "p" ).html( "<b>Single:</b> " + singleValues +
-        " <b>Multiple:</b> " + multipleValues.join( ", " ) );
-    console.log(multipleValues);
+    // // INSERT JAVASCRIPT CODE SAMPLE HERE
+    // //alert('Ready');
+    // $('h1').css('font-size', '40px');
+    // $('.ul-class').css('color', 'green');
+    // // var value = $('#tittleNew').val();
+    // // alert(value);
+    // var multipleValues = $( "#multiple1" ).val() || [];
+    // alert(multipleValues);
+    // // When using jQuery 3:
+    // // var multipleValues = $( "#multiple" ).val();
+    // $( "p" ).html( "<b>Single:</b> " + singleValues +
+    //     " <b>Multiple:</b> " + multipleValues.join( ", " ) );
+    // console.log(multipleValues);
+
+$('.codeup').css('border', 'solid 1px red');
 });
