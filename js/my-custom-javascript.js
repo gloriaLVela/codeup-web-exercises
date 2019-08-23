c8a5648d7e631cde5de71b2768cba06eda8df1ae
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-08-23 12:37:03 -0500

    jQuery exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,18 @@
 "use strict";
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     // INSERT JAVASCRIPT CODE SAMPLE HERE
-    alert ('Ready');
-
+    //alert('Ready');
+    $('h1').css('font-size', '40px');
+    $('.ul-class').css('color', 'green');
+    // var value = $('#tittleNew').val();
+    // alert(value);
+    var multipleValues = $( "#multiple1" ).val() || [];
+    alert(multipleValues);
+    // When using jQuery 3:
+    // var multipleValues = $( "#multiple" ).val();
+    $( "p" ).html( "<b>Single:</b> " + singleValues +
+        " <b>Multiple:</b> " + multipleValues.join( ", " ) );
+    console.log(multipleValues);
 });
